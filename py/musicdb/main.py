5880d93ee58b459c0bf5bc6ece55b65bf95d5929
Joost Molenaar <j.j.molenaar@gmail.com>
2012-02-21 08:50:39 +0100

    main module

--- py/musicdb/main.py
+++ py/musicdb/main.py
@@ -4,7 +4,7 @@ import musicdb
 class MusicDBRouter(core.Router):
     dispatch = [
         (r'^/musicdb/$',                                    musicdb.ctrl.root.Root()),
-        (r'^/musicdb/login/$',                              musicdb.ctrl.login.Login('/musicdb/static', 'text/html')),
+        (r'^/musicdb/login/$',                              musicdb.ctrl.login.Login('/web', 'text/html')),
         (r'^/musicdb/artist\.xml$',                         musicdb.ctrl.artist.ArtistRoot()),
         (r'^/musicdb/artist/([1-9][0-9]*)$',                musicdb.ctrl.artist.ArtistEditForm()),
         (r'^/musicdb/artist/new$',                          musicdb.ctrl.artist.ArtistAddForm()),
@@ -24,12 +24,12 @@ class MusicDBRouter(core.Router):
         (r'^/musicdb/related/label/([1-9][0-9]*)\.png$',    musicdb.ctrl.related.LabelAsGraph()),
         (r'^/musicdb/discogs/search/$',                     musicdb.ctrl.discogs.DiscogsSearch()),
         (r'^/musicdb/discogs/release/$',                    musicdb.ctrl.discogs.DiscogsRelease()),
-        (r'^/musicdb/(mobile\.html)$',                      core.FileServer('/musicdb/static', 'application/xhtml+xml')),
-        (r'^/musicdb/([a-z_]+.xsl)$',                       core.FileServer('/musicdb/static', 'text/xsl')),
-        (r'^/musicdb/([a-z]+\.css)$',                       core.FileServer('/musicdb/static', 'text/css')),
-        (r'^/musicdb/([a-z]+\.js)$',                        core.FileServer('/musicdb/static', 'text/javascript')),
-        (r'^/musicdb/(jquery.+\.js$)',                      core.FileServer('/musicdb/static/jquery', 'text/javascript')),
-        (r'^/musicdb/(jquery.+\.css$)$',                    core.FileServer('/musicdb/static/jquery-css', 'text/css'))
+        (r'^/musicdb/(mobile\.html)$',                      core.FileServer('/web', 'application/xhtml+xml')),
+        (r'^/musicdb/([a-z_]+.xsl)$',                       core.FileServer('/web', 'text/xsl')),
+        (r'^/musicdb/([a-z]+\.css)$',                       core.FileServer('/web', 'text/css')),
+        (r'^/musicdb/([a-z]+\.js)$',                        core.FileServer('/web', 'text/javascript')),
+        (r'^/musicdb/(jquery.+\.js$)',                      core.FileServer('/web/jquery', 'text/javascript')),
+        (r'^/musicdb/(jquery.+\.css$)$',                    core.FileServer('/web/jquery-css', 'text/css'))
     ]
 
 app = MusicDBRouter()
