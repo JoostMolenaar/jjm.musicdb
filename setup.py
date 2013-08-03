e56d9cc50f0bf3a77f94201c713202d96fe17c81
Joost Molenaar <j.j.molenaar@gmail.com>
2013-08-03 08:37:20 +0200

    Rename jjm.musicdb to musicdb

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     author='Joost Molenaar',
     author_email='j.j.molenaar@gmail.com',
-    name='jjm.musicdb',
+    name='musicdb',
     packages=[
-        'jjm.musicdb', 
-        'jjm.musicdb.model', 
-        'jjm.musicdb.view', 
-        'jjm.musicdb.ctrl'
+        'musicdb', 
+        'musicdb.model', 
+        'musicdb.view', 
+        'musicdb.ctrl'
     ],
     url='https://github.com/JoostMolenaar/musicdb',
     version='0.1.0'
